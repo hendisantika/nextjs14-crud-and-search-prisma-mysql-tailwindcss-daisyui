ee2316b43cd0e242b3b1a794677ded3ec76f7fb2
hendisantika <hendisantika@yahoo.co.id>
2024-08-07 11:31:02 +0700

    Add saveEmployee in action.ts 🫸🌀✏️📗🐧🐳

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -7,3 +7,31 @@ const EmployeeSchema = z.object({
     email: z.string().min(6),
     phone: z.string().min(11),
 });
+
+export const saveEmployee = async (prevSate: any, formData: FormData) => {
+    const validatedFields = EmployeeSchema.safeParse(
+        Object.fromEntries(formData.entries())
+    );
+
+    if (!validatedFields.success) {
+        return {
+            Error: validatedFields.error.flatten().fieldErrors,
+        };
+    }
+
+    try {
+        await prisma.employee.create({
+            data: {
+                name: validatedFields.data.name,
+                email: validatedFields.data.email,
+                phone: validatedFields.data.phone,
+            },
+        });
+
+    } catch (error) {
+        return {message: "Failed to create new employee"};
+    }
+
+    revalidatePath("/employee");
+    redirect("/employee");
+};
