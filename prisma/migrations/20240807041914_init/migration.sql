7d4fa4c9003491f8df882b81792d3abbc1bf568b
hendisantika <hendisantika@yahoo.co.id>
2024-08-07 13:08:40 +0700

    Bug Fix codes 🫸🌀✏️📗🐧🐳

--- prisma/migrations/20240807041914_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-create TABLE "Employee" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "phone" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
