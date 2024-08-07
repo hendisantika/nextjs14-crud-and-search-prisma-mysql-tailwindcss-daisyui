c5d2a95f1070fb9f2f4ab8435b49f1dd78bad862
hendisantika <hendisantika@yahoo.co.id>
2024-08-07 11:19:29 +0700

    Add Employee migration 🫸🌀✏️📗🐧🐳

---
+++ prisma/migrations/20240807041914_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+create TABLE "Employee" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
